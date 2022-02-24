47f472433eb35fe148ddfb529c65fb028165be77
timnirmal <timnirmal@gmail.com>
2022-02-24 12:24:35 +0530

    Static Blog

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return(
+        <div>
+            <h1>
+                About NextJS
+            </h1>
+            <p>Application to Demonstrate NextJS Basics</p>
+        </div>
+    );
+}
+
+export default About;
